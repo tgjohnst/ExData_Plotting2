08387155d21509a1a02066e487b68206b93e0522
Timothy Johnstone <timothy.g.johnstone@gmail.com>
2015-05-05 10:37:36 -0400

    set up file structure

---
+++ scripts/plot1_totalEmissions.R
@@ -0,0 +1,17 @@
+## plot1.R
+## Analysis script for the second project of Coursera Data Science Series, 
+## Course 4: Exploratory Data Analysis (exdata-014)
+## Timothy Johnstone, May 2015
+
+# Set working directory
+# TO RUN THIS SCRIPT please modify the path to reflect your system's file structure
+setwd("~/Dropbox/Coursera/4_Exploratory_Data_Analysis/Projects/ExData_Plotting2")
+
+##########
+
+# Load in dataset files
+nei <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds")
+classifications <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
+
+##########
+
