628bb8c6126885f2d630f1f49c2fd66e40fb2be7
Timothy Johnstone <timothy.g.johnstone@gmail.com>
2015-05-16 22:16:54 -0400

    finished plot 2

--- scripts/plot2_totalEmissionsDecrease.R
+++ scripts/plot2_totalEmissionsDecrease.R
@@ -5,7 +5,7 @@
 
 # Set working directory
 # TO RUN THIS SCRIPT please modify the path to reflect your system's file structure
-setwd("~/Dropbox/Coursera/4_Exploratory_Data_Analysis/Projects/ExData_Plotting2")
+setwd("Z:/Lotus/Dropbox/Coursera/4_Exploratory_Data_Analysis/Projects/ExData_Plotting2")
 
 ##########
 
@@ -15,3 +15,26 @@ scc <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
 
 ##########
 
+# Get just baltimore emissions
+nei_Baltimore <- nei[nei$fips=="24510",]
+
+# Calculate total emissions for each year in Baltimore
+totalEmissions_Baltimore <- aggregate(Emissions ~ year, nei_Baltimore,sum)
+
+# Set up output device and plot
+png("plots/plot2.png",600,600)
+
+plot(
+  totalEmissions_Baltimore$year, totalEmissions_Baltimore$Emissions/1000, type='l',
+  main="Total PM2.5 emissions in Baltimore MD (24510) by year",
+  xlab="Year",
+  ylim=c(0,4), ylab="PM2.5 Emissions (thousands of tons)", 
+)
+
+points(totalEmissions_Baltimore$year, totalEmissions_Baltimore$Emissions/1000,pch=19)
+
+#add lines indicating the starting and ending value
+abline(h=totalEmissions_Baltimore$Emissions[1]/1000, lty=2, col='red')
+abline(h=totalEmissions_Baltimore$Emissions[4]/1000, lty=2, col='green')
+
+dev.off()
